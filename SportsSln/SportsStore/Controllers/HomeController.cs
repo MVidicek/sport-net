dbcc16aa551ca991ee96192d164aa08d144c84a8
MVidicek <mvidicek@protonmail.com>
2022-09-06 13:46:08 +0200

    Preparing the Controller

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
         public IActionResult Index()
         {
             return View();
--- SportsSln/SportsStore/Models/IStoreRepository.cs
+++ SportsSln/SportsStore/Models/IStoreRepository.cs
@@ -2,6 +2,6 @@
 {
     public class IStoreRepository
     {
-        IQueryable<Product> Products { get; }
+        public IQueryable<Product> Products { get; }
     }
 }
